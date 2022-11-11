53b4353aa96454983b122377f7319cbc6ffad544
Harsh Parmar <you@example.com>
2022-11-12 02:05:48 +0530

    uploaded

--- app/Http/Controllers/JobRequestController.php
+++ app/Http/Controllers/JobRequestController.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Storage;
 use File;
 use App\Models\JobCategory;
 use App\Models\JobRequest;
+use Http;
+use App\Models\Employee;
+
 class JobRequestController extends Controller
 {
     private JobRequesttRepsitory $jobRequestRepository;
@@ -57,6 +60,44 @@ class JobRequestController extends Controller
         ]);
 
         $job_request = $this->jobRequestRepository->createJobRequest($orderDetails);
+
+        $job_request = JobRequest::where('job_message_status',0)->first();
+        if($job_request){
+            $employee = Employee::where('status','0')->limit(1)->get();
+            foreach($employee as $k => $value){
+                $client = Client::where('id',$job_request->client_id)->first();
+                $job = JobCategory::where('id',$job_request->job_id)->first();
+                $curl = curl_init();
+                $curl1 = curl_init();
+
+                $message = $client->client_name.' has been required to work for '.$job->job_title.' '.$client->supervisor;
+                $number =  $value->contact_number;
+
+                $message .= "\n Please select below link to fill the form \n".route('front.job_request');
+                $link = route('front.job_request');
+                
+                curl_setopt_array($curl, array(
+                    CURLOPT_URL => "https://api.ultramsg.com/instance22910/messages/chat",
+                    CURLOPT_RETURNTRANSFER => true,
+                    CURLOPT_ENCODING => "",
+                    CURLOPT_MAXREDIRS => 10,
+                    CURLOPT_TIMEOUT => 30,
+                    CURLOPT_SSL_VERIFYHOST => 0,
+                    CURLOPT_SSL_VERIFYPEER => 0,
+                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                    CURLOPT_CUSTOMREQUEST => "POST",
+                    CURLOPT_POSTFIELDS => "token=7iqilksxb8xyctsv&to=$number&body=$message&priority=10&referenceId=",
+                    CURLOPT_HTTPHEADER => array(
+                        "content-type: application/x-www-form-urlencoded"
+                    ),
+                ));
+
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+
+                curl_close($curl);
+            }
+        }
 
         return redirect()->route('job_request.index')
         ->with('success', 'Job Request Added Successfully.');
