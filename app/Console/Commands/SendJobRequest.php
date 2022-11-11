220bc938afa46354dd18eb277f873b930b47321b
Harsh Parmar <you@example.com>
2022-11-12 01:59:10 +0530

    uploaded

--- app/Console/Commands/SendJobRequest.php
+++ app/Console/Commands/SendJobRequest.php
@@ -62,23 +62,6 @@ class SendJobRequest extends Command
                     ),
                 ));
 
-                
-                curl_setopt_array($curl1, array(
-                    CURLOPT_URL => "https://api.ultramsg.com/instance22910/messages/link",
-                    CURLOPT_RETURNTRANSFER => true,
-                    CURLOPT_ENCODING => "",
-                    CURLOPT_MAXREDIRS => 10,
-                    CURLOPT_TIMEOUT => 30,
-                    CURLOPT_SSL_VERIFYHOST => 0,
-                    CURLOPT_SSL_VERIFYPEER => 0,
-                    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                    CURLOPT_CUSTOMREQUEST => "POST",
-                    CURLOPT_POSTFIELDS => "token=7iqilksxb8xyctsv&to=$number&link=$link",
-                    CURLOPT_HTTPHEADER => array(
-                    "content-type: application/x-www-form-urlencoded"
-                    ),
-                ));
-
                 $response = curl_exec($curl);
                 $err = curl_error($curl);
 
@@ -88,17 +71,6 @@ class SendJobRequest extends Command
                     echo "cURL Error #:" . $err;
                 } else {
                     echo $response;
-                }
-
-                $response1 = curl_exec($curl1);
-                $err1 = curl_error($curl1);
-
-                curl_close($curl1);
-
-                if ($err) {
-                    echo "cURL Error #:" . $err1;
-                } else {
-                    echo $response1;
                 }
             }
         }
